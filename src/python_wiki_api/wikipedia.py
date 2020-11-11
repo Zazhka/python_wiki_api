7c657cb0969461676f3d18966f182b80adfaa473
= <=>
2020-11-11 23:29:19 +0300

    fix: import space added

--- src/python_wiki_api/wikipedia.py
+++ src/python_wiki_api/wikipedia.py
@@ -1,11 +1,10 @@
 import click
 import requests
 
-
 API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
 
 
-def random_page(language='en'):
+def random_page(language="en"):
     url = API_URL.format(language=language)
 
     try:
--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -2,6 +2,6 @@ from python_wiki_api import wikipedia
 
 
 def test_random_page_uses_given_language(mock_requests_get):
-    wikipedia.random_page(language='de')
+    wikipedia.random_page(language="de")
     args, _ = mock_requests_get.call_args
     assert "de.wikipedia.org" in args[0]
